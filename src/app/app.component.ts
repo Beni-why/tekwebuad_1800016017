16406129017db90915efc2601a7f0bd0a6ad5e26
User <beni1800016017@webmail.uad.ac.id>
2020-03-23 19:33:34 +0700

    tekweblanjut1800016017

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component2.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'tekweblanjut1800016017';
+}
